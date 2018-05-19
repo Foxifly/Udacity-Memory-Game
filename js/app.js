8386d1e97255443c09ce969a468b3dfae9a9931a
Lindsay Ciastko <LindsayCiastko@gmail.com>
2018-05-19 09:11:42 -0700

    Added preliminary code for the cards
    Defined the card variable, pushed them all to an array, and added a click event listener to the cards.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@
  * Create a list that holds all of your cards
  */
 
+const listOfCards = document.querySelectorAll(".card");
+//Returns a NodeList, not an array - need to loop across the list and push to an array.
+let cards = [];
+for (let i = 0; i < listOfCards.length; i++) {
+  cards.push(listOfCards[i]);
+}
 
 /*
  * Display the cards on the page
@@ -12,20 +18,24 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
-
+$(".card").on("click", function(event) {
+  alert("hello world");
+});
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
